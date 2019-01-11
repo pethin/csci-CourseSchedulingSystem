86e9bb05547d36e46b9f2d84a052eda717027d9f
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2019-01-11 00:22:52 -0500

    Add CourseSchedulingSystem.Tests

---
+++ CourseSchedulingSystem.Tests/Helpers/HtmlHelpers.cs
@@ -0,0 +1,44 @@
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Html.Dom;
+using AngleSharp.Io;
+
+namespace CourseSchedulingSystem.Tests.Helpers
+{
+    public static class HtmlHelpers
+    {
+        public static async Task<IHtmlDocument> GetDocumentAsync(HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var document = await BrowsingContext.New()
+                .OpenAsync(ResponseFactory, CancellationToken.None);
+            return (IHtmlDocument)document;
+
+            void ResponseFactory(VirtualResponse htmlResponse)
+            {
+                htmlResponse
+                    .Address(response.RequestMessage.RequestUri)
+                    .Status(response.StatusCode);
+
+                MapHeaders(response.Headers);
+                MapHeaders(response.Content.Headers);
+
+                htmlResponse.Content(content);
+
+                void MapHeaders(HttpHeaders headers)
+                {
+                    foreach (var (key, values) in headers)
+                    {
+                        foreach (var value in values)
+                        {
+                            htmlResponse.Header(key, value);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
