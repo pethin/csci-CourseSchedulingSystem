dd8c91fa2ab0d8b12d1652173e7b512067a5e8e2
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2019-01-14 16:42:03 -0500

    Remove GPDR cookie policy

--- CourseSchedulingSystem/Startup.cs
+++ CourseSchedulingSystem/Startup.cs
@@ -28,13 +28,6 @@ namespace CourseSchedulingSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
@@ -63,7 +56,6 @@ namespace CourseSchedulingSystem
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseAuthentication();
 
