259195d1eb2535b82f234b506fb2e9cbd90d362c
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2019-02-25 11:45:52 -0500

    Remove unnecessary Identity pages

--- CourseSchedulingSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ CourseSchedulingSystem/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -81,10 +81,6 @@ namespace CourseSchedulingSystem.Areas.Identity.Pages.Account
                     _logger.LogInformation("User logged in.");
                     return LocalRedirect(returnUrl);
                 }
-                if (result.RequiresTwoFactor)
-                {
-                    return RedirectToPage("./LoginWith2fa", new { ReturnUrl = returnUrl, RememberMe = Input.RememberMe });
-                }
                 if (result.IsLockedOut)
                 {
                     _logger.LogWarning("User account locked out.");
