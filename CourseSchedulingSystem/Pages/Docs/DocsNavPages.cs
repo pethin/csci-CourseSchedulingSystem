d8b260b4c3e60df782cf066a6e48c9836ed3fa03
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2019-04-01 22:50:55 -0400

    Initial user manual layout

---
+++ CourseSchedulingSystem/Pages/Docs/DocsNavPages.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CourseSchedulingSystem.Pages.Docs
+{
+    public static class DocsNavPages
+    {
+        public static string IndexNavClass(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Index");
+        }
+
+        public static string OtherPage(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "OtherPage");
+        }
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                             ?? Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
