86a65483116671891cf7f000eaf3f338c6126724
Rankin <damr2@gmail.com>
2019-04-08 15:22:38 -0400

    Added cshtml files to Docs as a user manual. Switched destination of User Manual button to a new tab. Tried to remove button from "Course Scheduling System" in navbar. Added images and an image folder for use in documentation.

--- CourseSchedulingSystem/Pages/Docs/DocsNavPages.cs
+++ CourseSchedulingSystem/Pages/Docs/DocsNavPages.cs
@@ -6,14 +6,88 @@ namespace CourseSchedulingSystem.Pages.Docs
 {
     public static class DocsNavPages
     {
+        //Button for front page of documentation
         public static string IndexNavClass(ViewContext viewContext)
         {
             return PageNavClass(viewContext, "Index");
         }
 
-        public static string OtherPage(ViewContext viewContext)
+        //Button for the main menu section of tutorial
+        public static string MainMenu(ViewContext viewContext)
         {
-            return PageNavClass(viewContext, "OtherPage");
+            return PageNavClass(viewContext, "Main Menu");
+        }
+
+        //Button for User Settings tutorial
+        public static string Settings(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "User Settings");
+        }
+
+        //Button for the Add and Manage section of tutorial
+        public static string AddManage(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Add/Manage");
+        }
+
+        //Button for Term tutorial
+        public static string Term(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Term");
+        }
+
+        //Button for instructional methods tutorial
+        public static string Methods(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Methods");
+        }
+
+        //Button for courses tutorial
+        public static string Courses(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Courses");
+        }
+
+        //Button for Subjects tutorial
+        public static string Subjects(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Subjects");
+        }
+
+        //Button for Schedule Types tutorial
+        public static string Schedule(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Schedule Types");
+        }
+
+        //Button for Attributes tutorial
+        public static string Attributes(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Attributes");
+        }
+
+        //Button for buildings tutorial
+        public static string Buildings(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Buildings");
+        }
+
+        //Button for rooms tutorial
+        public static string Rooms(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Rooms");
+        }
+
+        //Button for users tutorial
+        public static string Users(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Users");
+        }
+
+        //Button for groups tutorial
+        public static string Groups(ViewContext viewContext)
+        {
+            return PageNavClass(viewContext, "Groups");
         }
 
         private static string PageNavClass(ViewContext viewContext, string page)
