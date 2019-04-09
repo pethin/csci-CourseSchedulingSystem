20c6d284d5d87ddd08fe28db9052ed2a8ebbf963
JordanLawson <jordanp.lawson@gmail.com>
2019-04-08 20:43:21 -0400

    Generated New CRUD Pages

---
+++ CourseSchedulingSystem/Pages/Manage/RoomRoomCapabilities/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CourseSchedulingSystem.Data;
+using CourseSchedulingSystem.Data.Models;
+
+namespace CourseSchedulingSystem.Pages.Manage.RoomRoomCapabilities
+{
+    public class CreateModel : PageModel
+    {
+        private readonly CourseSchedulingSystem.Data.ApplicationDbContext _context;
+
+        public CreateModel(CourseSchedulingSystem.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["RoomId"] = new SelectList(_context.Room, "Id", "Number");
+        ViewData["RoomCapabilityId"] = new SelectList(_context.RoomCapability, "Id", "Name");
+            return Page();
+        }
+
+        [BindProperty]
+        public RoomRoomCapability RoomRoomCapability { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.RoomRoomCapability.Add(RoomRoomCapability);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ CourseSchedulingSystem/Pages/Manage/Rooms/Edit.cshtml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CourseSchedulingSystem.Data;
+using CourseSchedulingSystem.Data.Models;
+
+namespace CourseSchedulingSystem.Pages.Manage.Rooms
+{
+    public class EditModel : PageModel
+    {
+        private readonly CourseSchedulingSystem.Data.ApplicationDbContext _context;
+
+        public EditModel(CourseSchedulingSystem.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Room Room { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Room = await _context.Room
+                .Include(r => r.Building)
+                .Include(r => r.Capability).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Room == null)
+            {
+                return NotFound();
+            }
+           ViewData["BuildingId"] = new SelectList(_context.Building, "Id", "Code");
+           ViewData["CapabilityId"] = new SelectList(_context.RoomCapability, "Id", "Name");
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Room).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RoomExists(Room.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool RoomExists(Guid id)
+        {
+            return _context.Room.Any(e => e.Id == id);
+        }
+    }
+}
