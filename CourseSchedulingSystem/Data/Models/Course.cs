2f15743d7ef2e145987c197381e9817bdaaa5589
Rankin <damr2@gmail.com>
2019-04-10 17:55:36 -0400

    Changed Credit hour input box to increment whole numbers

--- CourseSchedulingSystem/Data/Models/Course.cs
+++ CourseSchedulingSystem/Data/Models/Course.cs
@@ -25,9 +25,9 @@ namespace CourseSchedulingSystem.Data.Models
         public Guid SubjectId { get; set; }
         public Subject Subject { get; set; }
 
-        //Only 3-4 characters allowed
+        //Only 3-4 characters allowed, 3 numbers, and 1 letter
         [Required]
-        [RegularExpression(@"^[0-9]{3}[a-zA-Z]{0,1}$", ErrorMessage = "Only three numbers followed by an optional letter.")]
+        [RegularExpression(@"^[0-9]{3}[a-zA-Z]{0,1}$", ErrorMessage = "Must be three numbers followed by an optional letter.")]
         public string Number
         {
             get => _number;
@@ -45,7 +45,7 @@ namespace CourseSchedulingSystem.Data.Models
 
         [Required]
         [Column(TypeName = "decimal(5, 3)")]
-        [Range(0.000, double.MaxValue, ErrorMessage = "Credit hours must be zero or greater")]
+        [Range(0, double.MaxValue, ErrorMessage = "Credit hours must be 0 or greater")]
         [Display(Name = "Credit Hours")]
         [DisplayFormat(DataFormatString = "{0:F3}", ApplyFormatInEditMode = true)]
         public decimal CreditHours { get; set; }
