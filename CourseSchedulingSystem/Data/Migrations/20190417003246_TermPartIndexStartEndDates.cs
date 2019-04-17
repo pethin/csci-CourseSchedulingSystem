0d399f27b95d87687c5e642e6180673fbef955a1
Peter Nguyen <peter.hieu.nguyen@gmail.com>
2019-04-16 20:36:37 -0400

    Index term part start and end dates

---
+++ CourseSchedulingSystem/Data/Migrations/20190417003246_TermPartIndexStartEndDates.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CourseSchedulingSystem.Data.Migrations
+{
+    public partial class TermPartIndexStartEndDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_TermParts_EndDate",
+                table: "TermParts",
+                column: "EndDate");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TermParts_StartDate",
+                table: "TermParts",
+                column: "StartDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TermParts_EndDate",
+                table: "TermParts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TermParts_StartDate",
+                table: "TermParts");
+        }
+    }
+}
